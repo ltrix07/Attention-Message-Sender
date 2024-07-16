a88db061dc95a7722a2dae8cde62a61420c8691a
ltrix07 <skotarenko138@gmail.com>
2024-07-16 18:43:18 +0300

    drop

---
+++ maneger.py
@@ -0,0 +1,15 @@
+from attention_sender.utils import read_json
+
+def add_shop():
+    pass
+
+
+def maneger():
+    print(
+        'Actions:\n'
+        '1. Add shop'
+    )
+
+
+if __name__ == '__main__':
+    maneger()
