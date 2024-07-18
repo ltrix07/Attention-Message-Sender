2eb9c937b9ce962bc101cd1fca63b35e78370206
ltrix07 <skotarenko138@gmail.com>
2024-07-18 22:57:55 +0300

    add bad supplier check

--- attention_sender/errors.py
+++ attention_sender/errors.py
@@ -1,7 +1,6 @@
 async def google_sheet_err_proc(g_proc_res: list | dict) -> str | None:
     if isinstance(g_proc_res, dict):
-        if g_proc_res.get('forbidden'):
-            return None
-    elif not g_proc_res:
-        return None
+        if g_proc_res.get('errors'):
+            if 'forbidden' in g_proc_res.get('errors'):
+                return None
     return 'ok'
