546f4b4ac7cc1d28430964674d308623cea9c033
ltrix07 <skotarenko138@gmail.com>
2024-07-30 21:18:37 +0300

    async db

--- attention_sender/db.py
+++ attention_sender/db.py
@@ -5,50 +5,53 @@ from aiogram.types import Message
 
 class DataBase:
     def __init__(self, db_ph: str):
-        self.db_ph = db_ph
         self.conn = None
+        self.db_ph = db_ph
 
-    async def __aenter__(self):
-        self._check_dir()
+    async def initialize(self):
+        await self._check_dir()
         self.conn = await aiosqlite.connect(self.db_ph)
         await self._create_db()
+
+    async def __aenter__(self):
+        await self.initialize()
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
         await self.close_connection()
 
     @staticmethod
-    def _check_dir() -> None:
+    async def _check_dir() -> None:
         if not os.path.isdir('./cech'):
             os.mkdir('./cech')
 
     async def _create_db(self) -> None:
-        await self.conn.execute('''
-        CREATE TABLE IF NOT EXISTS sent_messages (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            message_id INTEGER,
-            chat_id INTEGER,
-            shop_name TEXT,
-            message_type TEXT,
-            order_id TEXT NULL,
-            text TEXT
-        )
-        ''')
-        await self.conn.commit()
+        async with self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS sent_messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                message_id INTEGER,
+                chat_id INTEGER,
+                shop_name TEXT,
+                message_type TEXT,
+                order_id TEXT NULL,
+                text TEXT
+            )
+        '''):
+            await self.conn.commit()
 
     async def close_connection(self) -> None:
         await self.conn.close()
 
     async def sent_mes_save(self, message: Message, shop_name: str, order: str, mes_type: str) -> None:
         try:
-            await self.conn.execute(
+            async with self.conn.execute(
                 '''
                 INSERT INTO sent_messages (message_id, chat_id, shop_name, message_type, order_id, text)
                 VALUES (?, ?, ?, ?, ?, ?)
                 ''',
                 (message.message_id, message.chat.id, shop_name, mes_type, order, message.text)
-            )
-            await self.conn.commit()
+            ):
+                await self.conn.commit()
         except aiosqlite.Error as e:
             print(f"Error occurred: {e}")
             await self.conn.rollback()
@@ -57,14 +60,14 @@ class DataBase:
         keys = [key for key in kwargs.keys()]
         values = [value for value in kwargs.values()]
         try:
-            await self.conn.execute(
+            async with self.conn.execute(
                 f'''
                 DELETE FROM sent_messages
                 WHERE {" = ? AND ".join(keys)} = ?
                 ''',
                 values
-            )
-            await self.conn.commit()
+            ):
+                await self.conn.commit()
         except aiosqlite.Error as e:
             print(f"Error occurred: {e}")
             await self.conn.rollback()
@@ -73,16 +76,16 @@ class DataBase:
         keys = [key for key in kwargs.keys()]
         values = [value for value in kwargs.values()]
         try:
-            cursor = await self.conn.execute(
+            async with self.conn.execute(
                 f'''
                 SELECT 1 FROM sent_messages
                 WHERE {" = ? AND ".join(keys)} = ?
                 LIMIT 1
                 ''',
                 values
-            )
-            row = await cursor.fetchone()
-            return row is not None
+            ) as cursor:
+                row = await cursor.fetchone()
+                return row is not None
         except aiosqlite.Error as e:
             print(f"Error occurred: {e}")
             return False
@@ -91,28 +94,28 @@ class DataBase:
         keys = [key for key in kwargs.keys()]
         values = [value for value in kwargs.values()]
         try:
-            cursor = await self.conn.execute(
+            async with self.conn.execute(
                 f'''
                 SELECT {desired_col} FROM sent_messages
                 WHERE {" = ? AND ".join(keys)} = ?
                 LIMIT 1
                 ''',
                 values
-            )
-            res = await cursor.fetchone()
-            if res:
-                return res[0]
-            else:
-                return None
+            ) as cursor:
+                res = await cursor.fetchone()
+                if res:
+                    return res[0]
+                else:
+                    return None
         except aiosqlite.Error as e:
             print(f"Error occurred: {e}")
             return False
 
     async def fetch_all_data(self) -> list:
         try:
-            cursor = await self.conn.execute('SELECT * FROM sent_messages')
-            rows = await cursor.fetchall()
-            return rows
+            async with self.conn.execute('SELECT * FROM sent_messages') as cursor:
+                rows = await cursor.fetchall()
+                return rows
         except aiosqlite.Error as e:
             print(f"Error occurred: {e}")
             return []
