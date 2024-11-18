dafbd50b75dd2eb93bdbdd0b12b883efd306163d
ltrix07 <skotarenko138@gmail.com>
2024-11-18 18:05:12 +0200

    fix sending no sheet

--- attention_sender/errors.py
+++ attention_sender/errors.py
@@ -2,10 +2,11 @@ async def google_sheet_err_proc(g_proc_res: list | dict) -> str | None:
     if isinstance(g_proc_res, dict):
         if g_proc_res['status'] == 'error' and g_proc_res.get('errors'):
             for error in g_proc_res.get('errors'):
-                if 'forbidden' in error:
-                    return 'forbidden'
-                if 'http_error' in error:
-                    return 'bad_req'
+                for key, _ in error.keys():
+                    if 'forbidden' in key:
+                        return 'forbidden'
+                    if 'http_error_500' in key:
+                        return 'bad_req'
     elif isinstance(g_proc_res, list) and len(g_proc_res) == 0:
         return 'bad_req'
     return 'ok'
