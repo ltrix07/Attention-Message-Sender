41b5503f95cbb9bf56ef169d6dd90015eec33170
ltrix07 <skotarenko138@gmail.com>
2024-12-05 21:40:47 +0200

    fix values error

--- attention_sender/errors.py
+++ attention_sender/errors.py
@@ -2,7 +2,7 @@ async def google_sheet_err_proc(g_proc_res: list | dict) -> str | None:
     if isinstance(g_proc_res, dict):
         if g_proc_res['status'] == 'error' and g_proc_res.get('errors'):
             for error in g_proc_res.get('errors'):
-                for key, _ in error.keys():
+                for key in error.keys():
                     if 'forbidden' in key:
                         return 'forbidden'
                     if 'http_error_500' in key:
